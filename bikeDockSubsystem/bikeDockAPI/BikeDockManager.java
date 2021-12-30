54ad9f546f8690f417acdcfa0827d50662f83c1e
lolislayer <nam09368@gmail.com>
2021-12-30 16:59:49 +0700

    v0.3

---
+++ bikeDockSubsystem/bikeDockAPI/BikeDockManager.java
@@ -0,0 +1,11 @@
+package bikeDockSubsystem.bikeDockAPI;
+import java.util.List;
+
+import bikeDockSubsystem.*;
+import model.*;
+
+public class BikeDockManager implements IBikeDockSubsystem{
+    public List<BikeDock> getDockList(){
+        return null;
+    }
+}
