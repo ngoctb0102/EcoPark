e9b27012b3c701d7e86d59bf8b4830cd59f20d06
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-01-03 00:33:39 +0700

    add something

--- view/bank/finalPayment/ReturnPayment.java
+++ view/bank/finalPayment/ReturnPayment.java
@@ -1,6 +1,7 @@
 package view.bank.finalPayment;
 import java.sql.Date;
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Calendar;
 public class ReturnPayment extends IPayment{
 
@@ -11,6 +12,6 @@ public class ReturnPayment extends IPayment{
     @Override
     public void completeLastStep() {
         //update status with bikeCode (licensePlate), userId, status = 0 (No need updating Date)
-        paymentController.saveRentBikeHistory(userId, bikeCode,0,new Date(Calendar.getInstance().getTime().getTime()));
+        paymentController.saveRentBikeHistory(userId, bikeCode,0,new Timestamp(Calendar.getInstance().getTime().getTime()));
     }
 }
