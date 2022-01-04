c11c023dad2f01a00b9841423164eaa365f3dbdd
dinhtuan-zzz <55046908+dinhtuan-zzz@users.noreply.github.com>
2022-01-04 09:25:01 +0700

    Create checkDateInputBlackBox.java

---
+++ test/MaiDInhTuanTest/checkDateInputBlackBox.java
@@ -0,0 +1,23 @@
+package test.MaiDinhTuanTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import controller.AddPageController;
+
+public class checkDateInputBlackBox {
+	AddPageController controller = new AddPageController();
+	
+	@Test
+	public void checkDateInput()  {
+		Object testDate[][] = {{true,"04:56:10"}, {false, "25:04:10"},{false, "04-56-10"}, {false, "-1"}};
+		for(int i = 0; i < testDate.length; ++i) {
+			assertEquals(testDate[i][0], controller.checkdate((String)testDate[i][1]));
+		}
+		
+	}
+	
+	
+
+}
