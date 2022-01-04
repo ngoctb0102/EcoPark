40ec1b279f44c2183d6c88e0fa3c5f79aa848952
dinhtuan-zzz <55046908+dinhtuan-zzz@users.noreply.github.com>
2022-01-04 09:25:37 +0700

    Create checkDateInputWhiteBox.java

---
+++ test/MaiDInhTuanTest/checkDateInputWhiteBox.java
@@ -0,0 +1,20 @@
+package test.MaiDinhTuanTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import controller.AddPageController;
+
+public class checkDateInputWhiteBox {
+	AddPageController controller = new AddPageController();
+	
+	@Test
+	public void checkDateInput()  {
+		Object testDate[][] = {{true,"04:56:10"}, {false, "-1"}};
+		for(int i = 0; i < testDate.length; ++i) {
+			assertEquals(testDate[i][0], controller.checkdate((String)testDate[i][1]));
+		}
+		
+	}
+}
